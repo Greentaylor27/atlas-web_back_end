db05f699ba54f9b800afa23a946d399f5254de0c
Greentaylor27 <greentaylor18@gmail.com>
2024-10-28 10:25:18 -0500

    I'm crashing out

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
-    task;
-    task2;
   }
 
   return [task, task2];
