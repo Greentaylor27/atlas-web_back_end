6b69faf61369cfbc835683fad19c09b47898fda9
Greentaylor27 <greentaylor18@gmail.com>
2024-10-30 12:01:22 -0500

    Gonna send this to see what I'm suppose to do

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft){
+    if (sqft !== 'number' || sqft <= 0) {
+      throw new Error('sqft must be a valid number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (sqft !== 'number' || newSqft <= 0) {
+      throw new Error('newSqft must be a valid number');
+    }
+    this._sqft = newSqft;
+  }
+
+  
+}
