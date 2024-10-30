c8cefbec7801237ac787faed88004dbb0affea32
Greentaylor27 <greentaylor18@gmail.com>
2024-10-30 10:14:14 -0500

    fixing lint issues

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
-  constructor(name, length, students) {
+  constructor(name, length, students){
 
-    if (typeof name !== 'string'){
-      throw new Error("Name must be a valid string!");
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a valid string!');
     } else {
       this._name = name;
     }
@@ -11,25 +11,18 @@ export default class HolbertonCourse {
     } else {
       this._length = length;
     }
-    if (!Array.isArray(students)){
+    if (!Array.isArray(students)) {
       throw new Error('invalid students: must be array');
     } else {
       this._students = students;
     }
   }
 
-  //getters
-  get name(){
+  // getters
+  get name() {
     return this._name;
   }
-  get length() {
-    return this._length;
-  }
-  get students() {
-    return this._students;
-  }
 
-  //setters
   set name(newName) {
     if (typeof newName === 'string'){
       this._name = newName;
@@ -38,12 +31,21 @@ export default class HolbertonCourse {
     }
   }
 
+  get length() {
+    return this._length;
+  }
+
   set length(newLength) {
     if (typeof newLength === 'number' || 0 >= newLength) {
       this._length = newLength;
     } else {
       throw new Error('newLength must be a valid number');
     }
+  }
+
+
+  get students() {
+    return this._students;
   }
 
   set students(newStudents) {
