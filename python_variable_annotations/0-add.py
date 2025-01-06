05142d2e8517a26da9194e6a10f7eabf95db5243
Greentaylor27 <greentaylor18@gmail.com>
2025-01-06 10:35:30 -0600

    Adding documentation to 0-add.py Adding pycache file to gitignore

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two floats together to return a float
+
+    Args:
+        a (float): A number
+        b (float): A number
+
+    Returns:
+        float: A number
+    """
     return a + b
