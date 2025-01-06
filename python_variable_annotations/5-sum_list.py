bb59379bdd007c62f27326e831b4628a88f0f180
Greentaylor27 <greentaylor18@gmail.com>
2025-01-06 11:47:09 -0600

    Attempting complex type annotation

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+5-sum_list.py
+
+First attempt at complex type annotation
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Takes a list of floats and adds them together
+
+    Args:
+        input_list (List[float]): a list of floats to be added together
+
+    Returns:
+        float: sum of the list
+    """
+    return sum(input_list)
