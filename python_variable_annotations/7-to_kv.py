f67c6a5a0590eda79be0550dff3f1149977b92e6
Greentaylor27 <greentaylor18@gmail.com>
2025-01-06 12:06:10 -0600

    First attempt at even more complex annotation

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Module takes a string and an int or float as an argument and returns a tuple
+"""
+from typing import Tuple, Union, Optional
+
+
+def to_kv(k: str, v: Union[int | float]) -> Tuple:
+    """
+    Method that takes two arguments and returns them as a tuple
+
+    Args:
+        k (str): A word or sentence
+        v (Union[int  |  float]): A number
+
+    Returns:
+        Tuple: A tuple containing k and v
+    """
+    
+    return tuple(k, v)
