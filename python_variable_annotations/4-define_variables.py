3b5efa81747788135fd5f0abd8d7a458e9d87112
Greentaylor27 <greentaylor18@gmail.com>
2025-01-06 11:38:57 -0600

    Defining variables annotations

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+4-define_variables.py
+
+Defines variables with values and annotations
+"""
+
+
+a: int = 1;
+pi: float = 3.14;
+i_understand_annotations: bool = True;
+school: str = "Holberton";
