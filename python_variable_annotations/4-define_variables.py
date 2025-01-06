883dbd2b577be21da0362c05557a9669d3081b4a
Greentaylor27 <greentaylor18@gmail.com>
2025-01-06 11:39:45 -0600

    python doesn't require semicolon at the end of each statement

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -7,7 +7,7 @@ Defines variables with values and annotations
 """
 
 
-a: int = 1;
-pi: float = 3.14;
-i_understand_annotations: bool = True;
-school: str = "Holberton";
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
