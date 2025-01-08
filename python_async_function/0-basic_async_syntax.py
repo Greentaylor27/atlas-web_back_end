927eb71dca87d267a2bce8eb18c9c49f7b910131
Greentaylor27 <greentaylor18@gmail.com>
2025-01-08 13:15:26 -0600

    Fixing some formatting issues I think that are causing issues with the checker

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,15 +3,19 @@
 """
 Module used to practice basic asynchronous coroutine
 """
-import random, asyncio
+import random
+import asyncio
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     Method to wait a random delay of time
 
     Args:
-        max_delay (int): 
+        max_delay (int): An integer
+        
+    Returns:
+        (float): A random number
     """
         
     delay = random.uniform(0, max_delay)
