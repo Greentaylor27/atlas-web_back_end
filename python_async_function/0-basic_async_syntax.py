dabf2c5ab472259105b4d5416aef4dfc636e92e3
Greentaylor27 <greentaylor18@gmail.com>
2025-01-09 10:11:53 -0600

    Pycodestyle Validation fixes

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -13,11 +13,11 @@ async def wait_random(max_delay: int = 10) -> float:
 
     Args:
         max_delay (int): An integer
-        
+
     Returns:
         (float): A random number
     """
-        
+
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
