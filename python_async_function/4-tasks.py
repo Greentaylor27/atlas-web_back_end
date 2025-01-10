dc52c57a9909d61333f8f44c3beca65cb821e6f7
Greentaylor27 <greentaylor18@gmail.com>
2025-01-10 11:07:33 -0600

    Seems to work

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+More Task practice
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Method used to complete multiple tasks concurrently
+
+    Args:
+        n (int): A number of time coroutine is to be executed
+        max_delay (int): Time delay in seconds
+
+    Returns:
+        List[float]: Returns a list of time taken
+    """
+
+    tasks = asyncio.create_task([wait_random(max_delay) for _ in range(n)])
+    results = asyncio.gather(*tasks)
+
+    return sorted(results)
