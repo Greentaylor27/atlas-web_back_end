4c67ee3f06403b57ae6655f88d2eb70e3125b86e
Greentaylor27 <greentaylor18@gmail.com>
2025-01-10 09:42:43 -0600

    Got task to complete multiple times will come back if I must to remove sorted() from return

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,9 +5,10 @@ Module used to practice multiple coroutines at the same time with async
 """
 import asyncio
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Takes wait_random and does it a number of time for a certain delay
 
@@ -19,7 +20,7 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
         List[float]: List of floats
     """
     
-    wait_random = __import__('0-basic_async_syntax').wait_random
-    delay = wait_random(n)
-    await asyncio.sleep(delay)
-    return delay
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    
+    return sorted(results)
