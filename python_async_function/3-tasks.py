b6f449f43fe6206d08e86cc4f0795e0ce216c850
Greentaylor27 <greentaylor18@gmail.com>
2025-01-10 10:23:09 -0600

    Pretty sure this is it?

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Module used to practice Tasks
+"""
+import asyncio
+from typing import Awaitable
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Awaitable[float]:
+    """
+    Don't really quite know what this is suppose to do I just know it returns a task
+
+    Args:
+        max_delay (int): The number being passed off to wait_random
+
+    Returns:
+        Awaitable[float]: Not even sure this is the correct annotation for this return
+    """
+
+    coro = asyncio.run(wait_random(max_delay))
+    task = asyncio.create_task(coro)
+    return task
