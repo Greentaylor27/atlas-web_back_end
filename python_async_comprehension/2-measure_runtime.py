6eb467e44e2dc5a59748f3a25e5e258c8c5ebc76
Greentaylor27 <greentaylor18@gmail.com>
2025-01-12 22:29:41 -0600

    How I handled this in the last project but instead of returning the list returning the time it takes

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+More practice with Async Comprehension
+"""
+import asyncio
+comprehension = __import__('1-async_comprehension').async_comprehension
+import time
+
+
+async def measure_runtime() -> float:
+    """
+    Measure run time for list comprehension
+
+    Returns:
+        float: seconds it will take to complete
+    """
+
+    start_time = time.time()
+
+    tasks = [asyncio.create_task(comprehension()) for _ in range(4)]
+    results = await asyncio.gather(*tasks)
+
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    return total_time
