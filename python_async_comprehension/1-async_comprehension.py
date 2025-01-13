de7e549d455932c9a3248ab8ae6e8b70be13bb6a
Greentaylor27 <greentaylor18@gmail.com>
2025-01-12 21:54:05 -0600

    Just a test to see what the task is looking for

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Module used to practice Async Comprehemsions
+"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Takes the returns of generator and makes a list
+
+    Returns:
+        List[float]: A list
+    """
+    return [async_generator]
