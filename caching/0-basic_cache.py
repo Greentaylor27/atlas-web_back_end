6fc3b92d5de91722ec971208e5251680edea7fa6
Greentaylor27 <greentaylor18@gmail.com>
2025-01-14 11:41:15 -0600

    replacing or operand with and operand

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -25,7 +25,7 @@ class BasicCache(BaseCaching):
             key: Where something is stored i.e. ID
             item: What is being stored
         """
-        if key or item:
+        if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
