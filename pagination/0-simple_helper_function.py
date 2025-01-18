d1a4bfac26bcc0bf2557699887a88d8bb366dd14
Greentaylor27 <greentaylor18@gmail.com>
2025-01-18 11:56:37 -0600

    Turning this in to see what the task is asking me to do

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,21 @@
+#!/bin/usr/env python3
+
+"""
+Module used to practice Pagination
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Used to index the page to determine how big the page size is?
+
+    Args:
+        page (int): Have no clue
+        page_size (int): How big the page is?
+
+    Returns:
+        Tuple[int, int]: should return (start_index, end_index)
+    """
+
+    
