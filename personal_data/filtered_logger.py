1874361e3bbbeeb1235d3a17e909c7065f16f501
Greentaylor27 <greentaylor18@gmail.com>
2025-01-21 12:42:48 -0600

    Sending over my start to see what the task is asking me

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Module used to practice filtered logging
+"""
+import logging
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator:str) -> str:
+    """
+    
+
+    Args:
+        fields (List[str]): List of strings that need to be obfuscated
+        redaction (str): Sting used to replace the string
+        message (str): The log line
+        separator (str): a character to seperator the message in the log line
+
+    Returns:
+        str: The log message obfuscated
+    """
+    print("Starting now!")
+    
+    print("We are doing something!")
+    print("We have finished")
+
