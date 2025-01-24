5dc618e66dc16c7ac3308ed021c4194bb3641a98
Greentaylor27 <greentaylor18@gmail.com>
2025-01-24 11:14:20 -0600

    Fixing the nex pycodestyle errors

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,8 +8,8 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,\
-                separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, 
+                    separator: str) -> str:
     """
     obfuscate a data point
 
