6a9762ca28c75071633c67ce747f29df5c4e2cb4
Greentaylor27 <greentaylor18@gmail.com>
2025-01-24 11:15:48 -0600

    testing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,8 +8,7 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, 
-                    separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
     """
     obfuscate a data point
 
