1ef1e8efe129268a198924a33892f5acef857524
Greentaylor27 <greentaylor18@gmail.com>
2025-01-25 13:24:05 -0600

    Task 3 complete!

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""Module used for authentacation
+"""
+from flask import request
+from typing import List
+from typing import TypeVar
+
+
+class Auth:
+    """Class used to store authentactation methods
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Set up for a later task
+
+        Args:
+            path (str): Urls that require authentacation
+            excluded_paths (List[str]): These do not
+
+        Returns:
+            bool: Returns False
+        """
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """Set up for later task
+
+        Args:
+            request (_type_, optional): Flask object. Defaults to None.
+
+        Returns:
+            str: _description_
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user
+        """
+        return None
