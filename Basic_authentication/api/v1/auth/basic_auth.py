a9e1d6f1ae4d2facb4e603cb53d1700d78776b75
Greentaylor27 <greentaylor18@gmail.com>
2025-01-25 15:18:15 -0600

    Updating app to accept BasicAuth and made the BasicAuth class

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""Module used for Basic Authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Class used for basic authentication
+
+    Args:
+        Auth (class): Parent class
+    """
+
