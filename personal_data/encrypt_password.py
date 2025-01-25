a1c381e28db79cfe1a26e392f4d731baedfbb1ec
Greentaylor27 <greentaylor18@gmail.com>
2025-01-25 11:21:21 -0600

    Didn't realize I had misspelled one of my variables in my code block

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -19,3 +19,19 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
     return hash
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks the validity of a hashed password
+
+    Args:
+        hashed_password (bytes): Hashed password
+        password (str): password
+
+    Returns:
+        bool: True or False
+    """
+    bytes = password.encode('utf-8')
+    if bcrypt.checkpw(bytes, hashed_password):
+        return True
+    else:
+        return False
