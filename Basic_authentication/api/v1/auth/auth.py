6c79b34e07ddd2213a6ebae3b9599d0a5c826d47
Greentaylor27 <greentaylor18@gmail.com>
2025-01-25 14:12:51 -0600

    Local test seems to be working
    Seeing if checker likes my solution

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -22,17 +22,22 @@ class Auth:
         Returns:
             bool: Returns False
         """
+
         if path == None:
             return True
         if excluded_paths == None or excluded_paths == []:
             return True
         
+        if path.endswith("/"):
+            pass
+        else:
+            path = path + "/"
+        
         for items in excluded_paths:
             if items == path:
                 return False
-        
-
-        
+            else:
+                return True
 
 
     def authorization_header(self, request=None) -> str:
