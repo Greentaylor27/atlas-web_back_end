919eb8ab1735c8f3d093a2d8b6fbd1aeb5b4817d
Greentaylor27 <greentaylor18@gmail.com>
2025-01-27 09:10:22 -0600

    Attempting to complete 7.basic

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,20 @@ class BasicAuth(Auth):
     Args:
         Auth (class): Parent class
     """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extracts the bases64 of the authorization header
+
+        Args:
+            authorization_header (str): the basic authorization header
+
+        Returns:
+            str: the base64 of the authorization header
+        """
+        header_validation = isinstance(authorization_header, str)
+
+        if not header_validation:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        
+        return authorization_header[6:]
