6696ef3e7587b307268958a994127d3ab30fef67
Greentaylor27 <greentaylor18@gmail.com>
2025-01-27 14:36:58 -0600

    Creating class SessionAuth
    Updating app.py to accept AUTH_TYPE session_auth

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Class used for session authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+
+class SessionAuth(Auth):
+    """Class use for session authentication
+
+    Args:
+        Auth (Object): Class to be inherited from
+    """
+    pass
