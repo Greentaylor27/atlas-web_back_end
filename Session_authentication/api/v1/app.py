4184f307348756f3e14345071eddf687323d6935
Greentaylor27 <greentaylor18@gmail.com>
2025-01-28 09:16:26 -0600

    moved the list to it's own variable to make pycodestyle happy added a new condition to the first if statement

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -69,15 +69,18 @@ def before_request():
     Returns:
         _type_: Uncertain
     """
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
+
     if auth is None:
         return
 
     request.current_user = auth.current_user(request)
 
-    if auth and auth.require_auth(request.path, ['/api/v1/status/',
-                                                 '/api/v1/unauthorized/',
-                                                 '/api/v1/forbidden/']):
-        if auth.authorization_header(request) is None:
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) and auth.session_cookie(request):
             abort(401)
         if request.current_user is None:
             abort(403)
