6eb40ae2fbe422d5458889f88fa66e903db68dee
Greentaylor27 <greentaylor18@gmail.com>
2025-01-29 12:40:21 -0600

    Can't test locally for some reason so again testing against the checker

---
+++ user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""module used for user authentication
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class User(declarative_base()):
+    """User class to store data in the user table
+    """
+
+    __table__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_toke = Column(String(250))
