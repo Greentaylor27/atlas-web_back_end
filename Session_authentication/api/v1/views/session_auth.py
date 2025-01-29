c9c91132beb28e1e91db3f6fad274285ca5de5a8
Greentaylor27 <greentaylor18@gmail.com>
2025-01-29 10:02:08 -0600

    Testing against the checker to see if I'm heading in the right direction

---
+++ Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""Module for session authentication
+"""
+from flask import Response, request, jsonify
+from api.v1.views import app_views, User
+
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """login for site
+    """
+    email = request.form.get({'email': email})
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({ "error": "email missing" }), 400
+    if not password:
+        return jsonify({ "error": "password missing" }), 400
+    
+    try:
+        user = User.search('email')
+    except Exception:
+        return jsonify({ "error": "no user found for this email"}), 404
+    
+    
+
+
