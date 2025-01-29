9f48984724b0d74d522fc4f166b66175ff0899f2
Greentaylor27 <greentaylor18@gmail.com>
2025-01-29 11:31:32 -0600

    for some reason I can't test locally so I'm having to test against the checker

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -4,36 +4,43 @@
 """
 from flask import Response, request, jsonify, abort
 from api.v1.views import app_views, User
-
+import os
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
     """login for site
     """
+    from api.v1.app import auth
+
     email = request.form.get('email')
     password = request.form.get('password')
 
     if not email:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if not password:
-        return jsonify({ "error": "password missing" }), 400
-    
-    try:
-        user = User.search(email)
-    except Exception:
-        return jsonify({ "error": "no user found for this email"}), 404
-    
-    user = user[0]
+        return jsonify({"error": "password missing"}), 400
 
-    if not user.is_valid_password(password):
-        return jsonify({ "error": "wrong password" }), 401
-    
-    else:
-        from api.v1.app import auth
+    users = User.search({"email": email})
 
-        session = auth.create_session(user.id)
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
 
-        if not session:
-            abort(500)
-        user_json = user.to_json()
+    user = users[0]
+
+    valid_password = user.is_valid_password(password)
+
+    if not valid_password:
+        return jsonify({"error": "Wrong password"}), 401
+
+    session_id = auth.create_session(user.id)
+    if not session_id:
+        abort(500)
+
+    user_json = user.to_json()
+
+    response = jsonify(user_json)
+
+    session_name = os.getenv('SESSION_NAME', 'session_id')
+    response.set_cookie(session_name, session_id)
+    return response
