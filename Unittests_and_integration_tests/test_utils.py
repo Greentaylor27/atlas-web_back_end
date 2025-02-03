b8451d5eeaa6f173c0eef5f0a37d9c979ce5738e
Greentaylor27 <greentaylor18@gmail.com>
2025-02-03 12:27:04 -0600

    HAHA FINALLY GOT DOCKER TO WORK SLIGHTLY oh and I guess the tests all work haha

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 
-"""This module is not empty """
+"""Unnittesting"""
+from utils import access_nested_map
+import unittest
+from parameterized import parameterized
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Class used for unit testing Utils.py """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """This is test that I'm returning what I'm suppose to"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
