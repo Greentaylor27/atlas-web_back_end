d3818b270639a175372cd9f7c05fbabe4897c3c9
Greentaylor27 <greentaylor18@gmail.com>
2025-02-16 10:27:20 -0600

    Fixing type annotation errors

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,6 +3,7 @@
 """Module used to practice redis with"""
 import redis
 import uuid
+from typing import Union
 
 
 
@@ -12,7 +13,7 @@ class Cache():
         self._redis = redis.Redis(host='localhost', port=6379)
         self._redis.flushdb()
 
-    def store(self, data: any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
