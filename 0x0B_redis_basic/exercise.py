c6b22396b6f72f452dbc47799fff6906c5c7a48f
Greentaylor27 <greentaylor18@gmail.com>
2025-02-16 10:25:47 -0600

    I'm starting to understand a bit

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,11 +2,17 @@
 
 """Module used to practice redis with"""
 import redis
+import uuid
 
 
 
 class Cache():
     """Class used for redis"""
     def __init__(self):
-        self._redis = redis.Redis(host='localhost')
-        
+        self._redis = redis.Redis(host='localhost', port=6379)
+        self._redis.flushdb()
+
+    def store(self, data: any) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
