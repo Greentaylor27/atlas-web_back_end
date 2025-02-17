fa2445fea4c3326ed399c6c9de9d7fb4583aa0de
Greentaylor27 <greentaylor18@gmail.com>
2025-02-17 11:25:14 -0600

    Still learning the Syntax to SQL

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
 -- Listing all bands with Glam rock and ranking them by longevity
-SELECT band_name FROM metal_bands WHERE style = 'Glam rock'
-SELECT IFNULL(formed, 0) - IFNULL(split, 0) AS lifespan FROM metal_bands
+SELECT band_name, IFNULL(formed, 0) - IFNULL(split, 0) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
