b5c7f62018f625ec6da2ce3089ad7fa620bba5a1
Greentaylor27 <greentaylor18@gmail.com>
2025-02-17 09:43:31 -0600

    Forgot to include auto increment

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Creating a table of unique users
 CREATE TABLE IF NOT EXISTS users (
-   id INT NOT NULL PRIMARY KEY,
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    email VARCHAR(255) NOT NULL UNIQUE,
    name VARCHAR(255)
 )
