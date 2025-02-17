133c90e58672704347054d9f37c542d9b83d1d67
Greentaylor27 <greentaylor18@gmail.com>
2025-02-17 12:33:25 -0600

    HAHA took a minute but I figured out the error and of course it was a syntax error.

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Listing all bands with Glam rock and ranking them by longevity
-SELECT band_name, IFNULL(split, 0) - IFNULL(formed, 0) AS lifespan
+SELECT band_name, IFNULL(split, YEAR(CURDATE())) - formed AS lifespan 
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
