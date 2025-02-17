e28410e0800a121bf8dd50dfc5e89c266d447dc3
Greentaylor27 <greentaylor18@gmail.com>
2025-02-17 13:01:22 -0600

    Testing against the checker

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,13 +1,11 @@
 -- Creates a trigger that decreases quantity of an item adding a new order
 DELIMITER $$
-
 CREATE TRIGGER decease_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.quantity_ordered
-    WHERE item_id = NEW.item_id
-END $$
-
+    BEGIN
+        UPDATE items
+        SET quantity = quantity - NEW.quantity_ordered
+        WHERE items.name = NEW.item_name
+    END$$
 DELIMITER ;
