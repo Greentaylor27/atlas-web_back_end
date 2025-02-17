6d0d550425f17f482e01fdd39688073ce815f278
Greentaylor27 <greentaylor18@gmail.com>
2025-02-17 12:37:06 -0600

    Curious to see something

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Listing all bands with Glam rock and ranking them by longevity
-SELECT band_name, IFNULL(split, YEAR(CURDATE()) - 1) - formed AS lifespan 
+SELECT band_name, IFNULL(split, YEAR(CURDATE())) - formed AS lifespan 
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
