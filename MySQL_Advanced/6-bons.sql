899a2578dbd6364a1d51c387099efe11161e56d3
Greentaylor27 <greentaylor18@gmail.com>
2025-02-19 17:02:17 -0600

    Slamming this against the checker

---
+++ MySQL_Advanced/6-bons.sql
@@ -0,0 +1,22 @@
+-- Creating first procedure.
+DELIMITER $$
+CREATE PROCEDURE Addbonus(
+    user_id INT,
+    project_name VARCHAR(255),
+    score INT
+)
+    BEGIN
+        DECLARE project_id INT;
+        SELECT id INTO project_id FROM projects WHERE name = project_name;
+        IF project_id IS NULL THEN
+            INSERT INTO projects (name)
+            VALUES (project_name);
+
+            SELECT LAST_INSEWR_ID() into project_id
+        END IF;
+
+        INSERT INTO corrections (user_id, project_id, score)
+        VALUES (user_id, project_id, score);
+    END $$
+DELIMITER ;
+
