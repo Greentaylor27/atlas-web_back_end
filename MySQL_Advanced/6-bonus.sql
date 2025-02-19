2b7e63948631c97431af49a74d1406bf2da22f98
Greentaylor27 <greentaylor18@gmail.com>
2025-02-19 17:17:34 -0600

    maybe missing a semicolon is what is breaking everything

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -7,17 +7,21 @@ CREATE PROCEDURE Addbonus(
 )
     BEGIN
         DECLARE project_id INT;
-        SELECT id INTO project_id FROM projects WHERE name = project_name;
+        SELECT id INTO project_id
+        FROM projects
+        WHERE name = project_name;
         
         IF project_id IS NULL THEN
             INSERT INTO projects (name)
             VALUES (project_name);
 
-            SELECT LAST_INSERT_ID() into project_id
+            SELECT LAST_INSERT_ID() into project_id;
         END IF;
 
         INSERT INTO corrections (user_id, project_id, score)
         VALUES (user_id, project_id, score);
     END $$
 DELIMITER ;
+
+
 
