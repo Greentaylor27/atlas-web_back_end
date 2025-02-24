266401653bf8c97e4dace7d17a6892a05c9ddee7
Greentaylor27 <greentaylor18@gmail.com>
2025-02-24 08:35:51 -0600

    Sending over what I have to make sure everything is updated

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 """Method to insert a document """
+import pymongo
 
+def insert_school(mongo_collection, **kwargs):
+    """Method to insert a school into a database using pymongo"""
+    
