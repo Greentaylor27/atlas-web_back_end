94a372f0a6296ba3d588f4171b50ae6c7bb434f8
Greentaylor27 <greentaylor18@gmail.com>
2025-02-24 09:27:21 -0600

    Moved process.on out of the process.stdin.on
    it causes it to repeat depending on how many prompts you send through

--- Node_js/1-stdin.js
+++ Node_js/1-stdin.js
@@ -1,5 +1,8 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n")
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 process.stdin.on('data', data => {
+    data = data.toString().charAt(0).toUpperCase() + data.slice(1);
     console.log(`Your name is: ${data}`);
-    process.on('exit', )
-})
+});
+process.on('exit', () => {
+    process.stdout.write("This important software is now closing\n");
+});
