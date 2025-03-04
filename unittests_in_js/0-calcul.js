dde741b15fcd11662ef9988af0762e8646edb3ef
Greentaylor27 <greentaylor18@gmail.com>
2025-03-04 13:04:41 -0600

    Here is what I have and I got a 4/4 on the checker

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,5 +1,5 @@
 function calculateNumber(a, b) {
-    return Math.round(a) + Math.round(b);
+    return Math.floor(a) + Math.floor(b);
 }
 
 module.exports = calculateNumber;
--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,12 +1,37 @@
 const assert = require('assert');
+const calc = require('./0-calcul');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () =>  {
-    it('Should return 1 after rounding and summing 1.1 and 0', () => {
-        assert.strictEqual(calculateNumber(1.1, 0), 1); // first number being rounded
+    it('should round and sum 2.75 and 4', () => {
+        assert.strictEqual(calc(2.75, 4), 6); // first number being rounded
     });
 
-    it('Should return 1 after rounding and summing 0 and 1.1', () => {
-        assert.strictEqual(calculateNumber(0, 1.1), 1); // first number being rounded
+    it('should round and sum 3 and 9.65', () =>{
+        assert.strictEqual(calc(3, 9.65), 12); // second number being rounded
+    });
+
+    it('should round and sum 1.05 and 3.5', () => {
+        assert.strictEqual(calc(1.05, 3.5), 4); // both numbers being rounded
+    });
+
+    it('should round and sum 4 and 5', () => {
+        assert.strictEqual(calc(4, 5), 9); // no numbers being rounded
+    });
+
+    it('should round and sum -1 and 2', () =>{
+        assert.strictEqual(calc(-1, 2), 1); // no number rounding but first number is negative
+    });
+
+    it('should round and sum 1 and -2', () =>{
+        assert.strictEqual(calc(1, -2), -1); // no rounding second number is negative
+    });
+
+    it('should round and sum 1 and -1.5', () =>{
+        assert.strictEqual(calc(1, -1.5), -1); // second number needs rounding and is negative 
+    });
+
+    it('should round and sum -1 and 1.5', () =>{
+        assert.strictEqual(calc(-1.5, 1), -1); // first number needs rounding and is negative
     });
 });
