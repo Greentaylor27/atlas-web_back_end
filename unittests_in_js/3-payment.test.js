880e99a67329e926e6409846146e7322db0f7fd7
Greentaylor27 <greentaylor18@gmail.com>
2025-03-04 15:23:44 -0600

    After spending an hour and a half chasing I finally found out where I was going wrong.

---
+++ unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment')
+const assert = require('assert');
+
+
+describe('sendPaymentRequestToApi', () => {
+    it('should call utils.calculateNumber with correct arguments', () => {
+        const spy = sinon.spy(Utils, 'calculateNumber');
+
+        sendPaymentRequestToApi(100, 20);
+
+        assert(spy.calledOnceWith('SUM', 100, 20));
+        spy.restore();
+    });
+});
