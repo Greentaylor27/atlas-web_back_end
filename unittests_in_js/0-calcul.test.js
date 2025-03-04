8a07f33dc581ecf24a539d9ce764b3a4a022d6ff
Greentaylor27 <greentaylor18@gmail.com>
2025-03-04 11:43:51 -0600

    Updating gitignore and attempting first task of unittests in JS

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const calc = require('./0-calcul');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () =>  {
+    it('should round and sum 4 and 5', () => {
+        assert.strictEqual(calculateNumber(4, 5), 9);
+    });
+
+    it('should round and sum 1.05 and 3.5', () => {
+        assert.strictEqual(calculateNumber(1.05, 3.5), 5);
+    });
+
+    it('should round and sum 2.75 and 4', () => {
+        assert.strictEqual(calculateNumber(2.75, 4), 7);
+    })
+
+    it('should round and sum 3 and 9.964', () =>{
+        assert.strictEqual(calculateNumber(3, 9.964), 13);
+    })
+})
