6783289804a4242fe212c03ef9c400ec2118ef7a
Greentaylor27 <greentaylor18@gmail.com>
2025-03-04 12:47:50 -0600

    Don't know why I had the require('./0-') twice

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,9 +1,8 @@
 const assert = require('assert');
-const calc = require('./0-calcul');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () =>  {
     it('Should return 7 after rounding and summing 2.75 and 4', () => {
-        assert.strictEqual(calc(2.75, 4), 7); // first number being rounded
+        assert.strictEqual(calculateNumber(2.75, 4), 7); // first number being rounded
     });
 });
