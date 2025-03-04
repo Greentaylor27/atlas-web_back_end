9c2067cf3ae8dfac0f85766810fd03a9fda980b0
Greentaylor27 <greentaylor18@gmail.com>
2025-03-04 10:22:16 -0600

    Sending what I have so far to show Tomas tomorrow

--- Node_js/5-http.js
+++ Node_js/5-http.js
@@ -1,6 +1,19 @@
 const http = require('http');
+const path = process.argv[2];
+const count = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/plain' });
-    res
-});
+    if (!path) {
+        res.writeHead(404);
+        res.end('Not found');
+    }
+    else if (req.url === '/') {
+        res.end('Hello Holberton School!');
+    }
+    else if (req.url === '/students') {
+        console.log("This is the list of our students")
+        count(path);
+    }
+}).listen(1245);
+
+module.exports = app;
