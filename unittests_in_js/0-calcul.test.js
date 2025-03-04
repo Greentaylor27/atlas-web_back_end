f7ebe8271b2f6490819917a9295b9c29c47bcc29
Greentaylor27 <greentaylor18@gmail.com>
2025-03-04 12:58:21 -0600

    I think it wants us to round down

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -5,4 +5,8 @@ describe('calculateNumber', () =>  {
     it('Should return 1 after rounding and summing 1.1 and 0', () => {
         assert.strictEqual(calculateNumber(1.1, 0), 1); // first number being rounded
     });
+
+    it('Should return 1 after rounding and summing 0 and 1.1', () => {
+        assert.strictEqual(calculateNumber(0, 1.1), 1); // first number being rounded
+    });
 });
