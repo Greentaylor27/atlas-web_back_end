0b30cc90d5d5a27f34623d422062b101cbd3364f
Greentaylor27 <greentaylor18@gmail.com>
2025-03-04 12:44:57 -0600

    adding Error picture to show Tomas just in case
    Working backwards and starting with the first number being rounded.

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -6,32 +6,4 @@ describe('calculateNumber', () =>  {
     it('should round and sum 2.75 and 4', () => {
         assert.strictEqual(calc(2.75, 4), 7); // first number being rounded
     });
-
-    it('should round and sum 3 and 9.65', () =>{
-        assert.strictEqual(calc(3, 9.65), 13); // second number being rounded
-    });
-
-    it('should round and sum 1.05 and 3.5', () => {
-        assert.strictEqual(calc(1.05, 3.5), 5); // both numbers being rounded
-    });
-
-    it('should round and sum 4 and 5', () => {
-        assert.strictEqual(calc(4, 5), 9); // no numbers being rounded
-    });
-
-    it('should round and sum -1 and 2', () =>{
-        assert.strictEqual(calc(-1, 2), 1); // no number rounding but first number is negative
-    });
-
-    it('should round and sum 1 and -2', () =>{
-        assert.strictEqual(calc(1, -2), -1); // no rounding second number is negative
-    });
-
-    it('should round and sum 1 and -1.5', () =>{
-        assert.strictEqual(calc(1, -1.5), 0); // second number needs rounding and is negative 
-    });
-
-    it('should round and sum -1 and 1.5', () =>{
-        assert.strictEqual(calc(-1.5, 1), 0); // first number needs rounding and is negative
-    });
 });
