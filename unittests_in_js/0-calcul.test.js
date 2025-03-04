3ec2b2ded5b2f339b2522fc596b10a37eb94bd1b
Greentaylor27 <greentaylor18@gmail.com>
2025-03-04 12:46:52 -0600

    Hopefully fixing checker issue?

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -3,7 +3,7 @@ const calc = require('./0-calcul');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () =>  {
-    it('should round and sum 2.75 and 4', () => {
+    it('Should return 7 after rounding and summing 2.75 and 4', () => {
         assert.strictEqual(calc(2.75, 4), 7); // first number being rounded
     });
 });
