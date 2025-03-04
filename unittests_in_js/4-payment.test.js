a016c1e825fdad6b25b9f00ef2bddbfe0e8f73dc
Greentaylor27 <greentaylor18@gmail.com>
2025-03-04 16:38:32 -0600

    Apparently did it with hooks first so sending this to have an example without hooks.

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -4,24 +4,17 @@ const sendPaymentRequestToApi = require('./4-payment');
 const assert = require('assert');
 
 describe('Payment Request', () => {
-    let consoleSpy;
-    let stub;
-
-    beforeEach(() => {
-        stub = sinon.stub(Utils, 'calculateNumber').returns(10);
-
-        consoleSpy = sinon.spy(console, 'log');
-    });
-
-    afterEach(() => {
-        stub.restore();
-        consoleSpy.restore();
-    });
-
+    
     it('should call calculateNumber with correct arguments and log the correct message', () => {
+        let consoleSpy = sinon.spy(console, 'log');
+        let stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
         sendPaymentRequestToApi(100, 20);
 
         assert(stub.calledWith('SUM', 100, 20));
-        assert(consoleSpy.calledWith('The total is: 10'))
-    })
+        assert(consoleSpy.calledWith('The total is: 10'));
+        
+        consoleSpy.restore();
+        stub.restore();
+    });
 });
