8e1ae5e4f277670f44191df2e01d15d821006b0b
Greentaylor27 <greentaylor18@gmail.com>
2025-03-05 09:49:23 -0600

    Still need to do the tests but want to make sure I send over my express routes

---
+++ unittests_in_js/9-api/api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 7865
+const welcomeMessage = `Welcome to the payment system`;
+const portMessage = `API available on localhost port ${port}`
+
+app.get('/', (req, res) => {
+    res.send(welcomeMessage);
+});
+
+app.get('/cart/:id(/^\d+$)', (req, res) => {
+    if (req.params.id) {
+        res.status(200).send(`Payment methods for cart ${id}\n`);
+    }
+    else {
+        res.status(404).send('id must be a number\n');
+    }
+});
+
+app.listen(port, () => {
+    console.log(portMessage)
+});
