e8e442668158a22e4c6639be91e0b84f7aa74f9f
Greentaylor27 <greentaylor18@gmail.com>
2025-03-05 09:58:17 -0600

    Removing the newline characters

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -12,11 +12,9 @@ app.get('/cart/:id', (req, res) => {
     const id = req.params.id;
 
     if (/^\d+$/.test(id)) {
-        res.status(200).send(`Payment methods for cart ${id}\n`);
+        res.status(200).send(`Payment methods for cart ${id}`);
     }
-    else {
-        res.status(404).send('id must be a number\n');
-    }
+    res.status(404).send('id must be a number');
 });
 
 app.listen(port, () => {
